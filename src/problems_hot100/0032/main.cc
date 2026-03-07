b55fc22ce78d1d7e522e43c3ae6148ba224f2676
Albresky <albre02@outlook.com>
2026-03-07 16:45:25 +0800

    [hard] #0032 DP Vector String

---
+++ src/problems_hot100/0032/main.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int longestValidParentheses(string s) {
+    int n = s.length();
+    vector<int> dp(n, 0);
+    int ans = 0;
+    for (int i = 1; i < n; ++i) {
+      if (s[i] == ')') {
+        if (s[i - 1] == '(')
+          dp[i] = (i - 2 >= 0 ? dp[i - 2] : 0) + 2;
+        else if (i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] == '(') {
+          dp[i] = (dp[i - 1] + 2) +
+                  (((i - dp[i - 1] - 2) >= 0) ? dp[i - dp[i - 1] - 2] : 0);
+        }
+        ans = max(ans, dp[i]);
+      }
+    }
+    return ans;
+  }
+};
+
+int main() {
+  string s = ")()())";
+  Solution solution;
+  cout << solution.longestValidParentheses(s) << endl;
+  return 0;
+}
